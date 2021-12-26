4fd111e0a522053f876208e53c029886519e5ef3
RtyC77 <76184927+RtyC77@users.noreply.github.com>
2021-12-26 00:50:22 -0300

    Tip-Calculator-Backup02
    O teste do backup passado funcionou certinho, essa é uma versão com o estilo somente para desktop de um dos lados finalizados ainda falta o lado da esquerda e fazer o site ficar responsivo para celular

--- Calculadora De Moeda/script.js
+++ Calculadora De Moeda/script.js
@@ -1,36 +1,48 @@
 function limpar() {
+    //Setando valor por valor = ""
+    document.getElementById("valorAtual").value = "";
+    document.getElementById("qtdPessoas").value = "";
+    document.getElementById("valorPorPessoaFinal").innerHTML = "";
+    document.getElementById("valorTotal").innerHTML = "";
+    document.getElementById("valorCustom").value = "";
 
     alert("Conteudo limpo com sucesso!");
 }
-
-function calcCinco(){
+//(escolha) vai pegar o valor da %, ex: 5%
+function calc(escolha){
     //Pegando o valor inserido dentro do input
     var valorTotal = document.getElementById("valorAtual").value;
+
+    //Fazendo o replace da virgula com o ponto
+    valorTotal = valorTotal.replace(",", ".");
+
     var qtdPessoas = document.getElementById("qtdPessoas").value;
-    console.log("Pegando o valor total: " + valorTotal);
-    console.log("Pegando o valor de pessoas: " + qtdPessoas);
+    //console.log("Pegando o valor total: " + valorTotal);
+    //console.log("Pegando o valor de pessoas: " + qtdPessoas);
 
-    var calculoPessoa = (valorTotal * 5 / 100) / qtdPessoas;
-    var calculoTotal = (valorTotal * 5 / 100) ;
-    console.log("Valor final da conta: " + calculoTotal);
-    console.log("Valor Calculado por pessoa" + calculoPessoa);
+    var calculoPessoa = ((valorTotal * escolha / 100) / qtdPessoas).toFixed(2);
+    var calculoTotal = (valorTotal * escolha / 100).toFixed(2) ;
+    //console.log("Valor final da conta: " + calculoTotal);
+    //console.log("Valor Calculado por pessoa" + calculoPessoa);
 
-    document.getElementById("valorPorPessoaFinal").innerHTML = "R$ " + calculoPessoa;
-    document.getElementById("ValorTotal").innerHTML = "R$ " + calculoTotal;
+    document.getElementById("valorPorPessoaFinal").innerHTML = calculoPessoa;
+    document.getElementById("valorTotal").innerHTML = calculoTotal;
 }
 
-function calcDez(){
-    //Pegando o valor inserido dentro do input
+function calcCustom() {
+    //pegando as infos básicas
+    var varvalorCustom = document.getElementById("valorCustom").value;
+    var valorCustomInt = parseInt(varvalorCustom);
+    console.log(valorCustomInt);
+
     var valorTotal = document.getElementById("valorAtual").value;
+    valorTotal = valorTotal.replace(",", ".");
+
     var qtdPessoas = document.getElementById("qtdPessoas").value;
-    console.log("Pegando o valor total: " + valorTotal);
-    console.log("Pegando o valor de pessoas: " + qtdPessoas);
 
-    var calculoPessoa = (valorTotal * 10 / 100) / qtdPessoas;
-    var calculoTotal = (valorTotal * 10 / 100) ;
-    console.log("Valor final da conta: " + calculoTotal);
-    console.log("Valor Calculado por pessoa" + calculoPessoa);
+    var calculoPessoa = ((valorTotal * valorCustomInt / 100) / qtdPessoas).toFixed(2);
+    var calculoTotal = (valorTotal * valorCustomInt / 100).toFixed(2) ;
 
-    document.getElementById("valorPorPessoaFinal").innerHTML = "R$ " + calculoPessoa;
-    document.getElementById("ValorTotal").innerHTML = "R$ " + calculoTotal;
+    document.getElementById("valorPorPessoaFinal").innerHTML = calculoPessoa;
+    document.getElementById("valorTotal").innerHTML = calculoTotal;
 }
